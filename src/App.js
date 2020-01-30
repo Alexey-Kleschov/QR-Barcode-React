922825059b755c2b165b447718f79645335b6c85
Alexey-Kleschov <iron-goga@yandex.ru>
2020-01-30 13:27:22 +0300

    install libs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+var QRCode = require('qrcode.react');
+var ReactDOM = require('react-dom');
+var Barcode = require('react-barcode');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // QR GENERATOR
+  const payload = {
+    name: 'Alex',
+    age: 29,
+    job: 'Programmer'
+  };
+  const data = JSON.stringify(payload);
+ return (
+  <div>
+    <QRCode value={data}  size={200}/>  
+    <Barcode value="12681436513658"  width={2}/>   
+  </div>  
+ );
 }
 
 export default App;
